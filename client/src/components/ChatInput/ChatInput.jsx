6cc3d2d0f203d8bbd2298bea530453181d6fa88f
Charudutta <restrictedcharudutta@gmail.com>
2022-08-31 23:24:42 +0530

    front end component added

---
+++ client/src/components/ChatInput/ChatInput.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./chatinput.scss";
+
+class ChatInput extends Component {
+  render() {
+    return (
+      <div className="ChatInput">
+        <input
+          onKeyDown={this.props.send}
+          placeholder="Enter a Message........"
+        />
+      </div>
+    );
+  }
+}
